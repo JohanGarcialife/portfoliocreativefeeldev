151513b4d833f0e15be5d8de581f7f77a66d3655
Johan <johan.garcia165@gmail.com>
2025-01-07 19:51:18 -0400

    Variables de entorno configuradas

--- supabase/Supabase.js
+++ supabase/Supabase.js
@@ -1,10 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://tmtuxumilpuoathgxaxn.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtdHV4dW1pbHB1b2F0aGd4YXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU0MzMxMDMsImV4cCI6MjA1MTAwOTEwM30.npjBIa3k74NHU3nlpADCTY724xbK8nB_1CBtQ3oyc3I";
+console.log(process.env.SUPABASE_URL);
 
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 
 export async function getTechnologiesApi() {
   const data = await supabase.from("technologies").select("*");
